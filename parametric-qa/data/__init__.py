ea062d8cded3371170825c8feb680874b015a407
Jae Heon Jung <108461006+heonyus@users.noreply.github.com>
2026-01-26 00:01:17 +0900

    feat: add data processing modules
    - download.py: NQ, HotpotQA, TriviaQA dataset downloaders
    - preprocess.py: corpus/QA pairs preprocessing pipeline
    - dataloader.py: WritePhaseDataset, ReadPhaseDataset classes

---
+++ parametric-qa/data/__init__.py
@@ -0,0 +1,3 @@
+from .download import download_dataset
+from .preprocess import preprocess_nq, preprocess_hotpotqa, build_corpus
+from .dataloader import WritePhaseDataset, ReadPhaseDataset, get_dataloader
